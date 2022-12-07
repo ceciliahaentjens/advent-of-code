d4b315dec4195f9428bf90c0cacd597672eae606
Cécilia Haentjens <75373556+ceciliahaentjens@users.noreply.github.com>
2022-12-07 09:50:39 +0100

    2022/day 5 (#5)
    * fix : Remove trim() from readData generic function
    * feat : Add Day 5 solutions

--- 2022/day-2/data.js
+++ 2022/day-2/data.js
@@ -3,6 +3,7 @@ const { readData } = require("../../helpers/input");
 // Read data
 const data = readData(__dirname)
   .split("\n")
+  .trim()
   .map((v) => v.split(" "));
 
 module.exports = { data };
--- 2022/day-3/data.js
+++ 2022/day-3/data.js
@@ -1,5 +1,5 @@
 const { readData } = require("../../helpers/input");
 
-const data = readData(__dirname).split("\n");
+const data = readData(__dirname).split("\n").trim();
 
 module.exports = { data };
